12cc8f9694dd5c2e5e4fd8c00793df14ba8641e0
evrenvural <evrenvural4@gmail.com>
2019-06-15 15:24:32 +0300

    Films were added to local storage

--- js/project.js
+++ js/project.js
@@ -6,6 +6,9 @@ const urlElement = document.querySelector("#url");
 // UI Objesini Başlatır
 const ui = new UI();
 
+// Storage Objesi Üret
+const storage = new Storage();
+
 // Events
 eventListeners();
 
@@ -17,15 +20,18 @@ function addFilm(e){
     const title = titleElement.value;
     const director = directorElement.value;
     const url = urlElement.value;
-
+    
+    // Hata durumu
     if(title === "" || director === "" || url === ""){
         ui.displayMessages("Tüm alanları doldurun...", "danger");
     }
+    // Yeni Film
     else{
         ui.displayMessages("Film eklendi.", "success");
         
         const newFilm = new Film(title, director, url);
-    
+        
+        storage.addFilmToStorage(newFilm);
         ui.addFilmToUI(newFilm);
     }
 
